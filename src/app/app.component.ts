40c2e8846ace420217ea31fd645c67fa68feb755
ItsMeEmmanuelG2 <itsmeemmadev@gmail.com>
2021-11-12 16:24:43 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+
+  nombre:string = 'emmAnuEl';
+
+  mostrarNombre():void {
+    console.log(this.nombre);
+    this.nombre = 'julio';
+    
+  }
 }
