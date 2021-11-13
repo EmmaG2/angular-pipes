04cf0a2a063c49a92139f92aaa7c189c51089641
ItsMeEmmanuelG2 <itsmeemmadev@gmail.com>
2021-11-12 21:32:42 -0600

    pages created

---
+++ src/app/ventas/pages/no-commons/no-commons.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-no-commons',
+  templateUrl: './no-commons.component.html',
+  styles: [
+  ]
+})
+export class NoCommonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/ventas/pages/order/order.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order',
+  templateUrl: './order.component.html',
+  styles: [
+  ]
+})
+export class OrderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
