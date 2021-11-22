a4b3ebf686b9be9198302fc28dc915495fe57332
ItsMeEmmanuelG2 <itsmeemmadev@gmail.com>
2021-11-22 14:49:47 -0600

    decimal pipe

--- src/app/ventas/pages/numbers/numbers.component.ts
+++ src/app/ventas/pages/numbers/numbers.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
 
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-numbers',
   templateUrl: './numbers.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class NumbersComponent implements OnInit {
+export class NumbersComponent {
+  header: string = 'Ventas Netas';
+  subheader: string = 'del año';
+  header2: string = 'Porcentaje';
+  subheader2: string = 'de ventas';
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ventasNetas: number = 2589402.585959;
+  porcentaje: number = 0.98;
 }
