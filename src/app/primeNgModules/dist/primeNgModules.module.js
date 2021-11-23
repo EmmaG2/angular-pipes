cba6450e01ae69207b8fa4d182a5ce78772cb0ef
ItsMeEmmanuelG2 <itsmeemmadev@gmail.com>
2021-11-23 15:37:04 -0600

    FieldsetModule added

---
+++ src/app/primeNgModules/dist/primeNgModules.module.js
@@ -0,0 +1,30 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+exports.__esModule = true;
+exports.PrimeNgModule = void 0;
+var core_1 = require("@angular/core");
+var button_1 = require("primeng/button");
+var card_1 = require("primeng/card");
+var menubar_1 = require("primeng/menubar");
+var fieldset_1 = require("primeng/fieldset");
+var PrimeNgModule = /** @class */ (function () {
+    function PrimeNgModule() {
+    }
+    PrimeNgModule = __decorate([
+        core_1.NgModule({
+            exports: [
+                button_1.ButtonModule,
+                card_1.CardModule,
+                menubar_1.MenubarModule,
+                fieldset_1.FieldsetModule
+            ]
+        })
+    ], PrimeNgModule);
+    return PrimeNgModule;
+}());
+exports.PrimeNgModule = PrimeNgModule;
--- src/app/primeNgModules/primeNgModules.module.ts
+++ src/app/primeNgModules/primeNgModules.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { MenubarModule } from 'primeng/menubar';
+import { FieldsetModule } from 'primeng/fieldset';
 
 @NgModule({
   exports: [
     ButtonModule,
     CardModule,
-    MenubarModule
+    MenubarModule,
+    FieldsetModule
   ]
 })
 export class PrimeNgModule { }
