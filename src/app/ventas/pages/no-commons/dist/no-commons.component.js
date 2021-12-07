ccaac3d70d9f5a8270c7bf0a4f12997a52ce1338
ItsMeEmmanuelG2 <itsmeemmadev@gmail.com>
2021-12-07 17:13:09 -0600

    no-commons updated

--- src/app/ventas/pages/no-commons/dist/no-commons.component.js
+++ src/app/ventas/pages/no-commons/dist/no-commons.component.js
@@ -8,38 +8,45 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 exports.__esModule = true;
 exports.NoCommonsComponent = void 0;
 var core_1 = require("@angular/core");
+var rxjs_1 = require("rxjs");
 var NoCommonsComponent = /** @class */ (function () {
     function NoCommonsComponent() {
         //*i18nSelect
         this.nombre = 'Emmanuel';
         this.genero = 'masculino';
         this.invitacionMap = {
-            'masculino': 'tenerlo',
-            'femenino': 'tenerla'
+            masculino: 'tenerlo',
+            femenino: 'tenerla'
         };
         //*i18nPlural
-        this.clientes = [
-            'k',
-            'hola',
-            'juan',
-            'xd',
-            'hola2'
-        ];
+        this.clientes = ['k', 'hola', 'juan', 'xd', 'hola2'];
         this.clientesMap = {
             '=0': 'No tenemos ningún cliente en sala de espera',
             '=1': 'Tenemos 1 cliente en sala de espera',
-            'other': 'Tenemos # clientes en sala de espera'
+            other: 'Tenemos # clientes en sala de espera'
         };
+        //* keyvalue pipe
+        this.persona = {
+            nombre: 'Emmanuel',
+            edad: 8,
+            direccion: 'Durango, Mexico'
+        };
+        //* async pipe
+        this.numbers = rxjs_1.interval(1000);
+        this.takeFourNumbers = this.numbers.pipe(rxjs_1.take(4));
+        this.valorPromesa = new Promise(function (resolve, reject) {
+            setTimeout(function () {
+                resolve('Tenemos data de la promesa');
+            }, 3500);
+        });
     }
     NoCommonsComponent.prototype.cambiarPersona = function () {
         this.nombre = 'Diana';
         this.genero = 'femenino';
     };
-    ;
     NoCommonsComponent.prototype.borrarPersona = function () {
         this.clientes.pop();
     };
-    ;
     NoCommonsComponent = __decorate([
         core_1.Component({
             selector: 'app-no-commons',
--- src/app/ventas/pages/no-commons/no-commons.component.ts
+++ src/app/ventas/pages/no-commons/no-commons.component.ts
@@ -1,43 +1,53 @@
 import { Component } from '@angular/core';
+import { interval, take } from 'rxjs';
 
 @Component({
-  selector   : 'app-no-commons',
+  selector: 'app-no-commons',
   templateUrl: './no-commons.component.html',
 })
-
 export class NoCommonsComponent {
-
   //*i18nSelect
-  nombre:string = 'Emmanuel';
-  genero:string = 'masculino';
+  nombre: string = 'Emmanuel';
+  genero: string = 'masculino';
 
   invitacionMap = {
-    'masculino': 'tenerlo',
-    'femenino' : 'tenerla',
+    masculino: 'tenerlo',
+    femenino: 'tenerla',
   };
 
   //*i18nPlural
-  clientes:string[] = [
-    'k',
-    'hola',
-    'juan',
-    'xd',
-    'hola2'
-  ];
+  clientes: string[] = ['k', 'hola', 'juan', 'xd', 'hola2'];
 
   clientesMap = {
-    '=0'   : 'No tenemos ningún cliente en sala de espera',
-    '=1'   : 'Tenemos 1 cliente en sala de espera',
-    'other': 'Tenemos # clientes en sala de espera',
+    '=0': 'No tenemos ningún cliente en sala de espera',
+    '=1': 'Tenemos 1 cliente en sala de espera',
+    other: 'Tenemos # clientes en sala de espera',
   };
 
   cambiarPersona() {
     this.nombre = 'Diana';
     this.genero = 'femenino';
-  };
+  }
 
   borrarPersona() {
     this.clientes.pop();
+  }
+
+  //* keyvalue pipe
+  persona = {
+    nombre: 'Emmanuel',
+    edad: 8,
+    direccion: 'Durango, Mexico',
   };
+
+  //* async pipe
+  numbers = interval(1000);
+  takeFourNumbers = this.numbers.pipe(take(4));
+
+  valorPromesa = new Promise( (resolve, reject) => {
+    setTimeout(() => {
+      resolve( 'Tenemos data de la promesa' )
+    }, 3500);
+  } );
 
 };
