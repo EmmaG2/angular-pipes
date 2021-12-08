67e1479a311629d3d507d08f2fe4d8e7c73b9301
ItsMeEmmanuelG2 <itsmeemmadev@gmail.com>
2021-12-07 23:52:15 -0600

    interfaces created

---
+++ src/app/ventas/interfaces/dist/ventas.interface.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+exports.Color = void 0;
+var Color;
+(function (Color) {
+    Color[Color["rojo"] = 0] = "rojo";
+    Color[Color["negro"] = 1] = "negro";
+    Color[Color["azul"] = 2] = "azul";
+    Color[Color["verde"] = 3] = "verde";
+})(Color = exports.Color || (exports.Color = {}));
---
+++ src/app/ventas/interfaces/ventas.interface.ts
@@ -0,0 +1,10 @@
+export enum Color {
+    rojo, negro, azul, verde
+}
+
+export interface Heroes {
+    nombre:string;
+    vuela:boolean;
+    color:Color;
+    
+}
